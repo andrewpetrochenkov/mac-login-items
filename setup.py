bd5fbf2afce5465abbf073e4fee4385eeba3c160
andrewp-as-is <russianidiot.github@gmail.com>
2020-10-22 16:14:08 +0300

    2020:10:22 16:14

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-login-items',
+    version='2020.10.22',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/login-items','scripts/login-items-add','scripts/login-items-names','scripts/login-items-paths','scripts/login-items-rm']
